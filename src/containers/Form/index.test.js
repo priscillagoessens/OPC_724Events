3ec344cf55dee62ffba5ae3718482543cf0311ab
priscilla goessens <priscillagoessens@gmail.com>
2024-03-19 15:30:04 +0100

    Modification des tests pour ajout de delai et modification du helper date pour l'index des mois

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Form from "./index";
 
 describe("When Events is created", () => {
@@ -22,7 +22,8 @@ describe("When Events is created", () => {
         })
       );
       await screen.findByText("En cours");
-      await screen.findByText("Envoyer");
+      // ajout d'un delai 
+      await waitFor(() => screen.findByText("Envoyer"), {timeout:2000});
       expect(onSuccess).toHaveBeenCalled();
     });
   });
