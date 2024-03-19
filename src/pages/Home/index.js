9c842384709576b9f5dba45d0c1066838cf34387
priscilla goessens <priscillagoessens@gmail.com>
2024-03-19 15:07:06 +0100

    Etiquette du footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,9 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const { data } = useData();
+  const last = data?.events[data.events.length - 1]; // on recupere la derniere info
+
   return <>
     <header>
       <Menu />
@@ -116,13 +118,15 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-        <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
-          date={new Date(last?.date)}
-          small
-          label="boom"
-        />
+        {last && ( // on verifie que last existe
+          <EventCard
+            imageSrc={last?.cover}
+            title={last?.title}
+            date={new Date(last?.date)}
+            small
+            label="boom"
+          />
+        )}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
