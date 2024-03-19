5374769f1304a4c40e41a45782a528369ede8a4f
priscilla goessens <priscillagoessens@gmail.com>
2024-03-19 15:13:52 +0100

    Modal de confirmation d'envoie du formulaire

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,8 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        // ajout de onSucces()
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
